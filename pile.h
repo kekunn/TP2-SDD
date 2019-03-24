3bd0981b61e7b469a34b1a4c9f64b9f3e4a856cb
root <root@DESKTOP-0OR30I7.localdomain>
2019-03-24 16:50:05 +0100

    Ajout des commentaires

--- pile.h
+++ pile.h
@@ -1,21 +1,21 @@
-#ifndef PILE_H
+#ifndef pile_H
 
-#define PILE_H
+#define pile_H
 #define FORMAT "%d\n"
 
 typedef int element_pile;
 
 typedef struct pile 
 {
-    int taille_max;
-    int rang_sommet;
+    int taille_max; //taille maximale de la pile
+    int rang_sommet; //rang du dernier element empile
     element_pile* valeurs;
 }
 pile_t;
 
-pile_t*  init_pile      (int taille);
-int      estVide        (pile_t *p);
-int      estPlein       (pile_t *p);
+pile_t*  initPile       (int taille);
+int      pileVide       (pile_t *p);
+int      pilePlein      (pile_t *p);
 int      empiler        (pile_t *p, element_pile valeur);
 void     depiler        (pile_t *p, element_pile *res, int *reussi);
 void     sommet         (pile_t *p, element_pile *res, int *reussi);
--- truc.h
+++ truc.h
@@ -1,7 +1,7 @@
 #ifndef TRUC_H
 #define TRUC_H
 
-void TRUC           (int i, int n, int* T);
+void TRUC_rec       (int i, int n, int* T);
 void TRUC_ite       (int i, int n, int* T);
 void Affichage      (int n, int * T);
 void Inverser       (int i, int j, int* T);
